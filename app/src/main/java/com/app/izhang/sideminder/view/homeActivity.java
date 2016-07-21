ccc17fda304ae8a523972d1193fb0cd2b691cd04
Ivan Zhang <ivanzhangemail@gmail.com>
2016-07-20 21:23:51 -0400

    FAB Button
    Started working on the FAB button.

--- app/src/main/java/com/app/izhang/sideminder/view/homeActivity.java
+++ app/src/main/java/com/app/izhang/sideminder/view/homeActivity.java
@@ -3,12 +3,14 @@ package com.app.izhang.sideminder.view;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -38,7 +40,12 @@ public class homeActivity extends AppCompatActivity implements homeView, Floatin
 
         addProjFab.setOnClickListener(this);
 
+        homeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+            }
+        });
     }
 
     @Override
@@ -84,5 +91,7 @@ public class homeActivity extends AppCompatActivity implements homeView, Floatin
     public void onClick(View v){
         // Assumes FAB button was clicked
         Toast.makeText(getApplicationContext(), "FAB button clicked", Toast.LENGTH_LONG).show();
+        AlertDialog.Builder addProjectPopup = new AlertDialog.Builder(this);
+
     }
 }
