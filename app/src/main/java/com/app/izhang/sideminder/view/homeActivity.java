8aa0ce56c959d25d5557d920d420e7b59314851a
Ivan Zhang <ivanzhangemail@gmail.com>
2016-07-26 21:31:46 -0400

    Calendar DatePicker & Spinner Implementation
    Implemented the date picker within the add project alert dialog. Also
    added spinner data and captures the content from user.

--- app/src/main/java/com/app/izhang/sideminder/view/homeActivity.java
+++ app/src/main/java/com/app/izhang/sideminder/view/homeActivity.java
@@ -1,5 +1,6 @@
 package com.app.izhang.sideminder.view;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,20 +16,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.izhang.sideminder.R;
 
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 public class homeActivity extends AppCompatActivity implements homeView, FloatingActionButton.OnClickListener{
 
+    static final String DATE_FORMAT = "EEE MMM dd HH:mm:ss z yyyy";
+
     private ListView homeList = null;
     private FloatingActionButton addProjFab = null;
+    private SimpleDateFormat sdf;
+    private DatePickerDialog deadlinePicker;
+    private EditText projDeadlineDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +63,8 @@ public class homeActivity extends AppCompatActivity implements homeView, Floatin
 
             }
         });
+
+        sdf = new SimpleDateFormat(DATE_FORMAT);
     }
 
     @Override
@@ -91,6 +106,21 @@ public class homeActivity extends AppCompatActivity implements homeView, Floatin
         return list;
     }
 
+    public String[] getIntervalList(){
+        String[] list = {
+            "1",
+                    "2",
+                    "3",
+                    "5",
+                    "7",
+                    "14",
+                    "30",
+                    "60",
+        };
+
+        return list;
+    }
+
     @Override
     public void onClick(View v){
         // Assumes FAB button was clicked
@@ -100,8 +130,33 @@ public class homeActivity extends AppCompatActivity implements homeView, Floatin
         LayoutInflater inflater = this.getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.addproject_dialog,null);
 
-        final TextView projNameTV = (TextView) dialogView.findViewById(R.id.projName);
-        final TextView projDescriptionTV = (TextView) dialogView.findViewById(R.id.projDesc);
+        final EditText projNameTV = (EditText) dialogView.findViewById(R.id.projName);
+        final EditText projDescriptionTV = (EditText) dialogView.findViewById(R.id.projDesc);
+        final Spinner projReminderInterval = (Spinner) dialogView.findViewById(R.id.projRemindInter);
+
+        // Spinner Populate
+        List<String> categories = Arrays.asList(getIntervalList());
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        projReminderInterval.setAdapter(dataAdapter);
+
+        // Deadline Init
+        projDeadlineDate = (EditText) dialogView.findViewById(R.id.projectDeadline);
+        projDeadlineDate.setInputType(InputType.TYPE_NULL);
+        projDeadlineDate.requestFocus();
+        projDeadlineDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDateTimeField();
+                deadlinePicker.show();
+            }
+        });
 
         dialogBuilder.setView(dialogView);
         dialogBuilder.setTitle("Add New Project");
@@ -110,9 +165,7 @@ public class homeActivity extends AppCompatActivity implements homeView, Floatin
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 //// TODO: 7/21/16 Add Project Data to Database
-                String projectName = projNameTV.getText().toString();
-                String projectDescription = projDescriptionTV.getText().toString();
-                Toast.makeText(getApplicationContext(), "Made new project, name: " + projectName, Toast.LENGTH_LONG).show();
+                saveData(projNameTV.getText().toString(), projDescriptionTV.getText().toString(),projReminderInterval.getSelectedItem().toString(), projDeadlineDate.getText().toString());
             }
         });
 
@@ -127,4 +180,22 @@ public class homeActivity extends AppCompatActivity implements homeView, Floatin
         alertDialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
         alertDialog.show();
     }
+
+
+    private void setDateTimeField() {
+        Calendar newCalendar = Calendar.getInstance();
+        deadlinePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                projDeadlineDate.setText(sdf.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    private void saveData(String projName, String projDescription, String projInterval, String projDeadline){
+        Log.v("Saving Data", "Project Name: " + projName + " \nProjection Description: " + projDescription + " \nProject Interval: " + projInterval + "\nProject Deadline: " + projDeadline);
+    }
+
 }
