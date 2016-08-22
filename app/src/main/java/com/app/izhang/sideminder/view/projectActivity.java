a58554a56215750995cfa184b3a2d7b32b0aec79
Ivan Zhang <ivanzhangemail@gmail.com>
2016-08-22 08:29:10 -0400

    Delete Project Impl
    * Added a delete option in project activity as part of the menu.

--- app/src/main/java/com/app/izhang/sideminder/view/projectActivity.java
+++ app/src/main/java/com/app/izhang/sideminder/view/projectActivity.java
@@ -1,11 +1,15 @@
 package com.app.izhang.sideminder.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.app.izhang.sideminder.R;
 import com.app.izhang.sideminder.model.Project;
+import com.app.izhang.sideminder.presenter.homePresenterImpl;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -14,6 +18,7 @@ import java.util.Date;
 public class projectActivity extends AppCompatActivity {
 
     static final String PROJECT_KEY = "PROJ_ID";
+    Project project = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,7 @@ public class projectActivity extends AppCompatActivity {
             throw new IllegalArgumentException("Activity does not contain project ID");
         }
 
-        Project project = Project.findById(Project.class, projID);
+        project = Project.findById(Project.class, projID);
 
         setTitle("Project: " + project.getTitle());
 
@@ -46,4 +51,33 @@ public class projectActivity extends AppCompatActivity {
                     hashTags));
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_project, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                if(project != null){
+                    homePresenterImpl presenter = new homePresenterImpl();
+                    presenter.removeProject(project.getId());
+                    Intent homeIntent = new Intent(getApplicationContext(), homeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
