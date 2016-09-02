cfc2663172991b0f1fc213e535bf4842ba59e7ee
Ivan Zhang <ivanzhangemail@gmail.com>
2016-09-01 22:26:26 -0400

    Deadline Setter
    * Added new alert dialog
    * Created calendar picker
    * Implemented presenter method to set the data received in the view

--- app/src/main/java/com/app/izhang/sideminder/presenter/projectPresenterImpl.java
+++ app/src/main/java/com/app/izhang/sideminder/presenter/projectPresenterImpl.java
@@ -1,5 +1,9 @@
 package com.app.izhang.sideminder.presenter;
 
+import com.app.izhang.sideminder.model.Project;
+
+import java.util.Date;
+
 /**
  * Created by ivanzhang on 8/31/16.
  */
@@ -11,7 +15,14 @@ public class projectPresenterImpl implements projectPresenter{
     }
 
     @Override
-    public boolean setDeadline(long projName, String projDeadline) {
-        return false;
+    public boolean setDeadline(long projID, String projDeadline) {
+        Project project = Project.findById(Project.class, projID);
+        if(project == null) return false;
+
+        Date date = new Date(projDeadline);
+        project.setDueDate(date);
+        project.save();
+
+        return true;
     }
 }
--- app/src/main/java/com/app/izhang/sideminder/view/projectActivity.java
+++ app/src/main/java/com/app/izhang/sideminder/view/projectActivity.java
@@ -1,5 +1,6 @@
 package com.app.izhang.sideminder.view;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -8,12 +9,14 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ContextThemeWrapper;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -22,24 +25,33 @@ import android.widget.Spinner;
 import com.app.izhang.sideminder.R;
 import com.app.izhang.sideminder.model.Project;
 import com.app.izhang.sideminder.presenter.homePresenterImpl;
+import com.app.izhang.sideminder.presenter.projectPresenterImpl;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class projectActivity extends AppCompatActivity {
 
     static final String PROJECT_KEY = "PROJ_ID";
-    Project project = null;
+    static final String DATE_FORMAT = "EEE MMM dd HH:mm:ss z yyyy";
+
+    private DatePickerDialog deadlinePicker;
+    private EditText projDeadlineDate;
+    private Project project = null;
+    private SimpleDateFormat sdf;
+    private long projID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
 
-        long projID = 0;
+        projID = 0;
 
         if(getIntent().hasExtra(PROJECT_KEY)){
             projID = getIntent().getLongExtra(PROJECT_KEY, 0);
@@ -47,6 +59,14 @@ public class projectActivity extends AppCompatActivity {
             throw new IllegalArgumentException("Activity does not contain project ID");
         }
 
+        sdf = new SimpleDateFormat(DATE_FORMAT);
+
+
+        initView();
+
+    }
+
+    private void initView(){
         project = Project.findById(Project.class, projID);
 
         setTitle("Project: " + project.getTitle());
@@ -60,11 +80,11 @@ public class projectActivity extends AppCompatActivity {
         final Date date = project.getDueDate();
 
         listview.setAdapter(new projectInfoListAdapter(this,
-                    sdf.format(date),
-                    Integer.toString(project.getReminderInterval()),
-                    hashTags,
-                    project.getDescription(),
-                    projID));
+                sdf.format(date),
+                Integer.toString(project.getReminderInterval()),
+                hashTags,
+                project.getDescription(),
+                projID));
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -85,40 +105,33 @@ public class projectActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
     public Dialog createDeadlinePresenter(Context context){
         AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.myDialog));
 
         LayoutInflater inflater = this.getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.addproject_dialog,null);
-        final EditText projNameTV = (EditText) dialogView.findViewById(R.id.projName);
-        final EditText projDescriptionTV = (EditText) dialogView.findViewById(R.id.projDesc);
-        final Spinner projReminderInterval = (Spinner) dialogView.findViewById(R.id.projRemindInter);
-        final EditText projDeadlineDate = (EditText) dialogView.findViewById(R.id.projectDeadline);
+        View dialogView = inflater.inflate(R.layout.setdeadline_dialog,null);
 
-        projNameTV.setVisibility(View.INVISIBLE);
-        projDescriptionTV.setVisibility(View.INVISIBLE);
-        projDeadlineDate.setVisibility(View.INVISIBLE);
-
-        // Spinner Populate
-        List<String> categories = Arrays.asList(getIntervalList());
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        projReminderInterval.setAdapter(dataAdapter);
+        // Deadline Init
+        projDeadlineDate = (EditText) dialogView.findViewById(R.id.projDeadline);
+        projDeadlineDate.setInputType(InputType.TYPE_NULL);
+        projDeadlineDate.requestFocus();
+        projDeadlineDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDateTimeField();
+            }
+        });
 
         builder.setView(dialogView);
         builder.setMessage("Change Deadline")
                 .setPositiveButton("Set", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
-
+                        projectPresenterImpl projectPresenter = new projectPresenterImpl();
+                        boolean didSetDeadline  = projectPresenter.setDeadline(projID, projDeadlineDate.getText().toString());
+                        if(didSetDeadline) initView();
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -131,10 +144,24 @@ public class projectActivity extends AppCompatActivity {
 
     }
 
+    private void setDateTimeField() {
+        Calendar newCalendar = Calendar.getInstance();
+        deadlinePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                projDeadlineDate.setText(sdf.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        deadlinePicker.show();
+    }
+
     public String[] getIntervalList(){
         String[] list = {
                 "1",
-                "2",
+                "2DSe",
                 "3",
                 "5",
                 "7",
